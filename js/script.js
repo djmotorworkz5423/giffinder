3688ca5fd63893e1f9cc1c37bf70d366231cee58
Damare Jerome <damaretheboy@gmail.com>
2018-03-14 21:16:43 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,64 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
+<!DOCTYPE html>
+<html>
 
-$(document).ready(function(){
-  
-  
-  
-  
-});
+  <head>
+      <title>
+        Giffinder
+      </title>
+      
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+      <script src="js/script.js" async></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <link type="text/css" rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+
+      <!-- Navigation -->
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <div class="container">
+              <div class="navbar-header">
+                  <a class="navbar-brand" href="#">Giffinder</a>
+              </div>
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <div class="col-sm-3 col-md-3 pull-right">
+                      <div class="navbar-form" role="search">
+                        <div class="input-group">
+                          <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+                          <div class="input-group-btn">
+                              <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+              </div>
+              <!-- /.navbar-collapse -->
+          </div>
+          <!-- /.container -->
+      </nav>
+
+      <!-- Page Content -->
+      <section>
+        <div class="container"> 
+            <div class="row gallery">
+                  <h3 class="text-center">  Search for a Gif above <br> Your results will display here</h3>
+            </div>
+        </div> 
+      <section>
+      <!-- Footer -->
+      <footer>
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-12">
+                      <p>Copyright &copy; Your Website 2017</p>
+                  </div>
+              </div>
+          </div>
+      </footer>   
+
+  </body>
+
+</html>
+
+//add 8080 for preveiw if there is an error
